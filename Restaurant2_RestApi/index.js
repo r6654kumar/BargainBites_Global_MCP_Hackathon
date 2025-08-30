7d59e761093ea0d08d3d39dbaee042cbfe1218fe
Rahul <rsc120@nist.edu>
2025-08-30 22:54:44 +0530

    db pointer for restaurant_1 added

--- Restaurant2_RestApi/index.js
+++ Restaurant2_RestApi/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 // import { MongoClient } from "mongodb";
-import {Client} from "pg";
+import { Client } from "pg";
 import dotenv from "dotenv";
 import menuRoutes from "./routes/menu.js";
 import orderRoutes from "./routes/order.js";
@@ -8,35 +8,57 @@ import offerRoutes from "./routes/offer.js"
 const app = express();
 app.use(express.json());
 dotenv.config();
-const PORT= process.env.PORT || 3002; //3002 for Restaurant 2 API
-const connectionString= process.env.PG_CONNECTION_STRING; 
-const client = new Client({ connectionString });
+const PORT = process.env.PORT || 3002; //3002 for Restaurant 2 API
+// const connectionString= process.env.PG_CONNECTION_STRING;
+const restaurant1_client = new Client({ connectionString: process.env.PG_CONNECTION_STRING_R1 });
+const restaurant2_client = new Client({ connectionString: process.env.PG_CONNECTION_STRING_R2 });
+// const restaurant2_client = new Client({ connectionString });
 // Connect to Postgress DB
 async function connectDB() {
     try {
-        await client.connect();
+        await restaurant2_client.connect();
         // const db = client.db("restaurant2_db");
-        console.log("Connect to Restaurant2_RestAPI Databse");
+        console.log("Connected to Restaurant2 Databse");
         // return db;
+        if (process.env.PG_CONNECTION_STRING_R1) {
+            await restaurant1_client.connect();
+            console.log("Connected to Restaurant1 Database");
+        }
     }
     catch (err) {
         console.log("Unable to connect to Postgress Database", err);
     }
 }
 
+//middleware for getting db connection based on headers....
+//if x-restaurant-id is passed db connection pointed to restaurant1, else restaurant2
+function getClient(req) {
+    return req.headers['x-restaurant-id'] === 'restaurant1' ? restaurant1_client : restaurant2_client;
+}
 
-//Changed to routes (using express router) --27/08
-app.use("/menu", menuRoutes(client));
-app.use("/order", orderRoutes(client));
-app.use("/offers", offerRoutes(client));
+//Changed to routes (using express router) --30/08
+app.use("/menu", (req, res, next) => {
+    const client = getClient(req);
+    menuRoutes(client)(req, res, next);
+});
+
+app.use("/order", (req, res, next) => {
+    const client = getClient(req);
+    orderRoutes(client)(req, res, next);
+});
+
+app.use("/offers", (req, res, next) => {
+    const client = getClient(req);
+    offerRoutes(client)(req, res, next);
+});
 
 
 //
 app.listen(PORT, async () => {
     await connectDB();
-    console.log("Restaurant2_RestAPI is running on PORT", PORT);
+    console.log("Restaurant API is running on PORT", PORT);
 })
 
-app.get("/", async(req,res)=>{
-    res.send("Server for Restaurant 2 running.........")
+app.get("/", async (req, res) => {
+    res.send("Server for Restaurant  running.........")
 })
